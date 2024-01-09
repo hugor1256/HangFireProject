bb2d9e5dca34654f11726963c4f8ae19a4389e6e
hmartins <hmartins@fazenda.ms.gov.br>
2024-01-09 08:12:32 -0400

    retirar comentarios

--- HangFireProject/Program.cs
+++ HangFireProject/Program.cs
@@ -7,17 +7,14 @@ builder.Services.AddHangfire(x =>
     x.UseSqlServerStorage(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<IEmailService, EmailService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
